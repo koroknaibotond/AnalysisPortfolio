1eefd51bb1c6c66a8e3856f5d0eb7055db3899e6
Botond Koroknai <127404741+koroknaibotond@users.noreply.github.com>
2025-08-10 22:53:31 +0200

    Delete Airline_data_analysis directory

--- Airline_data_analysis/R_scripts/Q2_effectofstops/avgprice_perstop.R
+++
@@ -1,8 +0,0 @@
-average_prices_and_stops <- flight_data %>%
-  group_by(airline, stops) %>% 
-  summarise(
-    Average_Price = mean(price, na.rm = TRUE)
-  ) %>%
-  arrange(desc(Average_Price))
-
-print(average_prices_and_stops) 
